8b8408d00ad19ddd24cd83c926c58f5bd43a0df2
Andrey Kutalev <akutalev@gmail.com>
2013-12-16 19:03:42 +0700

    Haml4j lib has been added.

--- src/main/java/com/recruiters/HelloController.java
+++ src/main/java/com/recruiters/HelloController.java
@@ -1,16 +1,34 @@
 package com.recruiters;
 
+import com.github.tangzero.haml4j.Template;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(final ModelMap model) {
+
+        File f = new File("/home/sevenbits/projects/hello.haml");
+        Template template = null;
+        try {
+            template = new Template(f);
+        } catch (IOException e) {
+
+        }
+
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("title", "Ham4J");
+        context.put("message", "Simple message");
+
+        return template.render(context);
+    }
 }
