e6e741e4b67a7e409e48cc3d135b590cab3ce55b
Dmitry Shmatko <leodex23@gmail.com>
2013-12-20 12:49:57 +0700

    Added controller for R11 Active Vacancies

--- src/main/java/com/recruiters/model/Vacancy.java
+++ src/main/java/com/recruiters/model/Vacancy.java
@@ -1,29 +1,53 @@
 package com.recruiters.model;
 
-import java.util.Date;
-import java.util.List;
-
 /**
  * Vacancy POJO Class
  */
 public class Vacancy {
-    private Long id;
-    private String title;
-    private String shortDescription;
-    private String longDescription;
-    private Date creationDate;
-    private Date expirationDate;
-    private String salary;
-    private Test test;
+    public Long id;
+    public String title;
+    public String shortDescription;
+    public String creationDate;
 
-    private List<Applicant> applicantList;
+    public Vacancy() {
+    }
 
-    private List<Recruiter> recruiterList;
-    // Can be null
-    private Recruiter activeRecruiter;
-    // Job search is over when is not null
-    private Applicant activeApplicant;
+    public Vacancy(Long id, String title, String shortDescription, String creationDate) {
+        this.id = id;
+        this.title = title;
+        this.shortDescription = shortDescription;
+        this.creationDate = creationDate;
+    }
 
-    // Should consider more flags: archive etc
-    private boolean isActive;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
 }
--- src/main/java/com/recruiters/web/controller/recruiter/ActiveVacancies.java
+++ src/main/java/com/recruiters/web/controller/recruiter/ActiveVacancies.java
@@ -1,19 +1,60 @@
 package com.recruiters.web.controller.recruiter;
 
+import com.recruiters.model.Vacancy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Controller Class for R11 "Show active vacancies"
+ */
 @Controller
 public class ActiveVacancies {
 
-    @RequestMapping(value = "/recruiterActiveVacancies", method = RequestMethod.GET)
+    /**
+     * Controller for R11 "Active vacancies list"
+     * @return model and view with list of active vacancies
+     */
+    @RequestMapping(value = "/recruiter/activeVacancies", method = RequestMethod.GET)
     public ModelAndView showActiveVacancies() {
 
-        ModelAndView mav = new ModelAndView("main");
-
-        return mav;
+        ModelAndView activeVacancies = new ModelAndView ("activeVacancies");
+        List<Vacancy> vacancyList = getActiveVacancies();
+        activeVacancies.addObject ("vacancyList", vacancyList);
+        return activeVacancies;
     }
+
+    /**
+     * Fake getActiveVacancies method, should be real service method of RecruiterService in future
+     * @return list of active vacancies
+     */
+    private List<Vacancy> getActiveVacancies () {
+        final Long VACANCY1_ID = 1L;
+        final String VACANCY1_TITLE = "Программист";
+        final String VACANCY1_SHORT_DESCRIPTION = "PHP guru";
+        final String VACANCY1_DATE = "сегодня";
+        final Long VACANCY2_ID = 2L;
+        final String VACANCY2_TITLE = "Лесоруб";
+        final String VACANCY2_SHORT_DESCRIPTION = "Умеет клёво рубить сосны";
+        final String VACANCY2_DATE = "сегодня";
+        final Long VACANCY3_ID = 4L;
+        final String VACANCY3_TITLE = "Сантехник";
+        final String VACANCY3_SHORT_DESCRIPTION = "Не пьёт!";
+        final String VACANCY3_DATE = "сегодня";
+        List<Vacancy> activeVacancies = new ArrayList<Vacancy>();
+        Vacancy vacancy1 = new Vacancy (VACANCY1_ID, VACANCY1_TITLE, VACANCY1_SHORT_DESCRIPTION, VACANCY1_DATE);
+        Vacancy vacancy2 = new Vacancy (VACANCY2_ID, VACANCY2_TITLE, VACANCY2_SHORT_DESCRIPTION, VACANCY2_DATE);
+        Vacancy vacancy3 = new Vacancy (VACANCY3_ID, VACANCY3_TITLE, VACANCY3_SHORT_DESCRIPTION, VACANCY3_DATE);
+        activeVacancies.add (vacancy1);
+        activeVacancies.add (vacancy2);
+        activeVacancies.add (vacancy3);
+        return activeVacancies;
+    }
+
+
 }
