eca03ce0d6f3d669e703f0f9290e6eee471d2275
Dmitry Shmatko <leodex23@gmail.com>
2013-12-20 16:54:52 +0700

    Url mapping modified to correct

--- src/main/java/com/recruiters/web/controller/recruiter/ShowVacancy.java
+++ src/main/java/com/recruiters/web/controller/recruiter/ShowVacancy.java
@@ -1,13 +1,13 @@
 package com.recruiters.web.controller.recruiter;
 
-import com.recruiters.model.VacancyFull;
+
+import com.recruiters.model.Vacancy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 /**
  * Controller Class for R2 "Show Vacancy"
@@ -19,21 +19,21 @@ public class ShowVacancy {
      * Controller for R2 "Show vacancy"
      * @return model and view with one vacancy
      */
-    @RequestMapping(value = "/recruiter/showVacancy/{$vacancyId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/recruiter-vacancy-show/{$vacancyId}", method = RequestMethod.GET)
     public ModelAndView showVacancyById (@PathVariable Long vacancyId) {
 
-        ModelAndView showVacancy = new ModelAndView ("showVacancy");
-        VacancyFull vacancy = getVacancyFullById (vacancyId);
+        ModelAndView showVacancy = new ModelAndView ("recruiter-vacancy-show.jade");
+        Vacancy vacancy = getVacancyById(vacancyId);
         showVacancy.addObject("vacancy", vacancy);
         return showVacancy;
     }
 
     /**
-     * Fake "get full vacancy description" method, should be implemented in RecruiterService
+     * Fake "get vacancy description" method, should be implemented in RecruiterService
      * @param vacancyId    Id of vacancy for which we want to get full description
-     * @return full vacancy description
+     * @return vacancy description
      */
-    private VacancyFull getVacancyFullById (Long vacancyId) {
+    private Vacancy getVacancyById (Long vacancyId) {
         final Long VACANCY1_ID = 1L;
         final String  VACANCY1_TITLE = "Лесоруб";
         final String VACANCY1_DESCRIPTION = "Обожаю рубить сосны!";
@@ -41,10 +41,10 @@ public class ShowVacancy {
         final String VACANCY1_CREATION_DATE = "сегодня";
         final String VACANCY1_EXPIRATION_DATE = "завтра";
         final Long VACANCY1_TEST_ID = 1L;
-        VacancyFull vacancyFull1 = new VacancyFull (VACANCY1_ID, VACANCY1_TITLE, VACANCY1_DESCRIPTION,
+        Vacancy vacancy = new Vacancy (VACANCY1_ID, VACANCY1_TITLE, VACANCY1_DESCRIPTION,
                 VACANCY1_SALARY, VACANCY1_CREATION_DATE, VACANCY1_EXPIRATION_DATE, VACANCY1_TEST_ID);
         if (vacancyId.equals(VACANCY1_ID)) {
-            return vacancyFull1;
+            return vacancy;
         } else {
             return null;
         }
