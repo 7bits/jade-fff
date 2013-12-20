7613bf3d07b47dd993b29966bb9b08c7c734361b
Dmitry Shmatko <leodex23@gmail.com>
2013-12-20 17:34:04 +0700

    Code styling changed to appropriate for 7bits

--- src/main/java/com/recruiters/web/controller/recruiter/ModifyApplicant.java
+++ src/main/java/com/recruiters/web/controller/recruiter/ModifyApplicant.java
@@ -2,7 +2,6 @@ package com.recruiters.web.controller.recruiter;
 
 import com.recruiters.web.form.ApplicantForm;
 import com.recruiters.web.validator.AddApplicantValidator;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -18,44 +17,54 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  */
 @Controller
 public class ModifyApplicant {
-
-    protected final Logger LOG = Logger.getLogger(getClass());
+    /** Apache Logger for this class*/
+    //protected final Logger logger = Logger.getLogger(getClass());
+    /** Validator for add applicant */
     @Autowired
-    AddApplicantValidator addApplicantValidator;
+    private AddApplicantValidator addApplicantValidator;
 
     /**
      * Controller for creating new applicant with method GET
      * @return model and view with empty applicant
      */
     @RequestMapping(value = "recruiter-employee-create/{vacancyId}", method = RequestMethod.GET)
-    public ModelAndView addApplicant (@PathVariable Long vacancyId) {
+    public ModelAndView addApplicant(@PathVariable final Long vacancyId) {
 
-        ModelAndView addApplicant = new ModelAndView ("recruiter-employee-create.jade");
+        ModelAndView addApplicant = new ModelAndView("recruiter-employee-create.jade");
         ApplicantForm applicantForm = new ApplicantForm();
-        applicantForm.setVacancyId (vacancyId);
-        addApplicant.addObject ("applicantForm", applicantForm);
+        applicantForm.setVacancyId(vacancyId);
+        addApplicant.addObject("applicantForm", applicantForm);
         return addApplicant;
     }
 
+    /**
+     * Controller for creating new applicant with method POST
+     * @param applicantForm         Form model attribute for applicant
+     * @param vacancyId             Id of vacancy for corresponded applicant
+     * @param bindingResult         BindingResult
+     * @param redirectAttributes    RedirectAttributes
+     * @return model and view for creating new applicant with errors if any,
+     * otherwise redirects to R6 "Vacancy in progress of job searching"
+     */
     @RequestMapping(value = "recruiter-employee-create/{vacancyId}", method = RequestMethod.POST)
-    public ModelAndView addApplicantValidation (@ModelAttribute("applicantForm")
-                                                    ApplicantForm applicantForm,
-                                                @PathVariable Long vacancyId,
-                                                BindingResult bindingResult,
-                                                RedirectAttributes redirectAttributes) {
+    public ModelAndView addApplicantValidation(@ModelAttribute("applicantForm")
+                                                   final ApplicantForm applicantForm,
+                                               @PathVariable final Long vacancyId,
+                                               final BindingResult bindingResult,
+                                               final RedirectAttributes redirectAttributes) {
 
-        addApplicantValidator.validate (applicantForm, bindingResult);
+        addApplicantValidator.validate(applicantForm, bindingResult);
         if (bindingResult.hasErrors()) {
-            ModelAndView model = new ModelAndView ("recruiter-employee-create.jade");
-            LOG.debug ("Adding applicant was not successful");
+            ModelAndView model = new ModelAndView("recruiter-employee-create.jade");
+            //LOG.debug ("Adding applicant was not successful");
             model.addObject("errors", bindingResult.getFieldError().getDefaultMessage());
             return model;
         }
         return new ModelAndView("redirect:/recruiter-progress-vacancy-show/" + vacancyId);
     }
-
-    private void addApplicantToDataBase (){
-
-    }
+//
+//    private void addApplicantToDataBase (){
+//
+//    }
 
 }
