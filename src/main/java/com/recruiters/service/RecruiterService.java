0b2abdabcac134002ce84a7fddf13f3d9120485d
Andrey Kutalev <akutalev@gmail.com>
2013-12-23 11:26:59 +0700

    Some refactoring. Logic from AvailableVacanciesController has been replaced to the recruiter service.

--- src/main/java/com/recruiters/service/RecruiterService.java
+++ src/main/java/com/recruiters/service/RecruiterService.java
@@ -1,6 +1,53 @@
 package com.recruiters.service;
 
+import com.recruiters.model.Vacancy;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  */
+@Service
 public class RecruiterService {
+
+    /** Id of 1st vacancy */
+    static final Long VACANCY1_ID = 1L;
+    /** Title of 1st vacancy */
+    static final String VACANCY1_TITLE = "Программист";
+    /** Description of 1st vacancy */
+    static final String VACANCY1_SHORT_DESCRIPTION = "PHP guru";
+    /** Date of 1st vacancy */
+    static final String VACANCY1_DATE = "сегодня";
+    /** Id of 2nd vacancy */
+    static final Long VACANCY2_ID = 2L;
+    /** Title of 2nd vacancy */
+    static final String VACANCY2_TITLE = "Лесоруб";
+    /** Description of 2nd vacancy */
+    static final String VACANCY2_SHORT_DESCRIPTION = "Умеет клёво рубить сосны";
+    /** Date of 2nd vacancy */
+    static final String VACANCY2_DATE = "сегодня";
+    /** Id of 3rd vacancy */
+    static final Long VACANCY3_ID = 4L;
+    /** Title of 3rd vacancy */
+    static final String VACANCY3_TITLE = "Сантехник";
+    /** Description of 3rd vacancy */
+    static final String VACANCY3_SHORT_DESCRIPTION = "Не пьёт!";
+    /** Date of 3rd vacancy */
+    static final String VACANCY3_DATE = "сегодня";
+
+
+    public List<Vacancy> findAvailableListOfVacancies() {
+
+        List <Vacancy> vacancies = new ArrayList<Vacancy>();
+
+        Vacancy vacancy1 = new Vacancy(VACANCY1_ID, VACANCY1_TITLE, VACANCY1_SHORT_DESCRIPTION, VACANCY1_DATE);
+        Vacancy vacancy2 = new Vacancy(VACANCY2_ID, VACANCY2_TITLE, VACANCY2_SHORT_DESCRIPTION, VACANCY2_DATE);
+        Vacancy vacancy3 = new Vacancy(VACANCY3_ID, VACANCY3_TITLE, VACANCY3_SHORT_DESCRIPTION, VACANCY3_DATE);
+        vacancies.add(vacancy1);
+        vacancies.add(vacancy2);
+        vacancies.add(vacancy3);
+
+        return vacancies;
+    }
 }
---
+++ src/main/java/com/recruiters/web/controller/recruiter/AvailableVacancies.java
@@ -0,0 +1,38 @@
+package com.recruiters.web.controller.recruiter;
+
+import com.recruiters.service.RecruiterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller Class for R11 "Show active vacancies"
+ */
+@Controller
+public class AvailableVacancies {
+
+    @Autowired
+    private RecruiterService recruiterService = null;
+
+    /**
+     * Controller for R11 "Active vacancies list"
+     * @return model and view with list of active vacancies
+     */
+    @RequestMapping(value = "recruiter-available-vacancies", method = RequestMethod.GET)
+    public ModelAndView showActiveVacancies() {
+        ModelAndView activeVacancies = new ModelAndView("recruiter-available-vacancies.jade");
+        activeVacancies.addObject("vacancyList", getRecruiterService().findAvailableListOfVacancies());
+
+        return activeVacancies;
+    }
+
+    public RecruiterService getRecruiterService() {
+        return recruiterService;
+    }
+
+    public void setRecruiterService(final RecruiterService recruiterService) {
+        this.recruiterService = recruiterService;
+    }
+}
