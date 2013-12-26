f2ec230b75c1e70c0e876f77ba6d272be431a00f
Dmitry Shmatko <leodex23@gmail.com>
2013-12-26 12:42:15 +0700

    employer-progress-vacancies-list.jade, corresponding controller and further files modified for repository DB

--- src/main/java/com/recruiters/service/EmployerService.java
+++ src/main/java/com/recruiters/service/EmployerService.java
@@ -3,6 +3,7 @@ package com.recruiters.service;
 import com.recruiters.model.Deal;
 import com.recruiters.model.Employer;
 import com.recruiters.model.User;
+import com.recruiters.model.Vacancy;
 import com.recruiters.repository.DealRepository;
 import com.recruiters.repository.EmployerRepository;
 import com.recruiters.repository.UserRepository;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  */
@@ -21,6 +23,7 @@ public class EmployerService {
     private EmployerRepository employerRepository = null;
     @Autowired
     private DealRepository dealRepository = null;
+
 
     /**
      * Detached user getter, should be combined with equal getter
@@ -56,5 +59,15 @@ public class EmployerService {
             return deal;
         }
         return null;
+    }
+
+    /**
+     * Gets all active deals from DB for current employer
+     * @param employer    Employer POJO instance
+     * @return list of deals
+     */
+    public List<Deal> findEmployerDeals(final Employer employer) {
+
+        return dealRepository.findAllActiveByEmployerId(employer.getId());
     }
 }
