c90ebc3021c6e26fcc00587bc9318e44e66acac4
Dmitry Shmatko <leodex23@gmail.com>
2013-12-28 13:04:21 +0700

    Applicant MyBatis mapping added w/o update method (TODO)

--- src/main/java/com/recruiters/repository/ApplicantRepository.java
+++ src/main/java/com/recruiters/repository/ApplicantRepository.java
@@ -2,6 +2,8 @@ package com.recruiters.repository;
 
 import com.recruiters.model.Applicant;
 import com.recruiters.model.Deal;
+import com.recruiters.repository.mapper.ApplicantMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -13,46 +15,13 @@ import java.util.List;
 @Repository
 public class ApplicantRepository {
 
-    /** Id of 1st applicant */
-    static final Long APPLICANT1_ID = 1L;
-    /** 1st Applicant first name */
-    static final String APPLICANT1_FIRST_NAME = "Иван";
-    /** 1st Applicant last name */
-    static final String APPLICANT1_LAST_NAME = "Иванов";
-    /** Id of 2nd applicant */
-    static final Long APPLICANT2_ID = 2L;
-    /** 2nd Applicant first name */
-    static final String APPLICANT2_FIRST_NAME = "Пётр";
-    /** 2nd Applicant last name */
-    static final String APPLICANT2_LAST_NAME = "Петров";
-    /** Id of 3rd applicant */
-    static final Long APPLICANT3_ID = 3L;
-    /** 3rd Applicant first name */
-    static final String APPLICANT3_FIRST_NAME = "Константин";
-    /** 3rd Applicant last name */
-    static final String APPLICANT3_LAST_NAME = "Константинопольский";
-
+    @Autowired
+    private ApplicantMapper applicantMapper = null;
 
     public Applicant getApplicantById(final Long applicantId) {
-        DealRepository dealRepository = new DealRepository();
-        Deal deal = dealRepository.getById(1L);
-        Applicant applicant1 = new Applicant(APPLICANT1_ID, deal,
-                APPLICANT1_FIRST_NAME, APPLICANT1_LAST_NAME, "Круто работает", "Мужской", 34, "#", "#");
-        Applicant applicant2 = new Applicant(APPLICANT2_ID, deal,
-                APPLICANT2_FIRST_NAME, APPLICANT2_LAST_NAME, "Работает с утра до ночи", "Мужской", 26, "#", "#");
-        Applicant applicant3 = new Applicant(APPLICANT3_ID, deal,
-                APPLICANT3_FIRST_NAME, APPLICANT3_LAST_NAME, "Работает не покладая рук", "Мужской", 41, "#", "#");
-        if(applicantId.equals(1L)) {
-            return applicant1;
-        }
-        if(applicantId.equals(2L)) {
-            return applicant2;
-        }
-        if(applicantId.equals(3L)) {
-            return applicant3;
-        }
 
-        return null;
+        Applicant applicant = applicantMapper.getApplicantById(applicantId);
+        return applicant;
     }
 
     /**
@@ -61,24 +30,9 @@ public class ApplicantRepository {
      * @return
      */
     public List<Applicant> getApplicantByDealId(final Long dealId) {
-        List<Applicant> applicantList = new ArrayList<Applicant>();
-        DealRepository dealRepository = new DealRepository();
 
-        Applicant applicant1 = new Applicant(APPLICANT1_ID, null,
-                APPLICANT1_FIRST_NAME, APPLICANT1_LAST_NAME);
-        Applicant applicant2 = new Applicant(APPLICANT2_ID, null,
-                APPLICANT2_FIRST_NAME, APPLICANT2_LAST_NAME);
-        Applicant applicant3 = new Applicant(APPLICANT3_ID, null,
-                APPLICANT3_FIRST_NAME, APPLICANT3_LAST_NAME);
-        applicantList.add(applicant1);
-        applicantList.add(applicant2);
-        applicantList.add(applicant3);
-        if (dealId.equals(1L)) {
-            return applicantList;
-        } else {
-            return null;
-        }
-
+        List<Applicant> applicantList = applicantMapper.getApplicantByDealId(dealId);
+        return applicantList;
     }
 
     /**
@@ -86,7 +40,16 @@ public class ApplicantRepository {
      * @param applicant
      * @return <tt>true</tt> if model has been saved successful, <tt>false</tt> - otherwise
      */
+    //TODO
     public Boolean saveApplicant(final Applicant applicant) {
         return true;
+    }
+
+    public ApplicantMapper getApplicantMapper() {
+        return applicantMapper;
+    }
+
+    public void setApplicantMapper(final ApplicantMapper applicantMapper) {
+        this.applicantMapper = applicantMapper;
     }
 }
---
+++ src/main/java/com/recruiters/repository/mapper/ApplicantMapper.java
@@ -0,0 +1,69 @@
+package com.recruiters.repository.mapper;
+
+import com.recruiters.model.Applicant;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * Mapper for Applicant POJO
+ */
+public interface ApplicantMapper {
+
+    @Select("SELECT applicants.*, " +
+            "vacancies.id as vacancy_id, " +
+            "vacancies.employer_id, " +
+            "recruiters.id as recruiter_id " +
+            "FROM applicants " +
+            "INNER JOIN deals ON deals.id=applicants.deal_id " +
+            "INNER JOIN vacancies ON vacancies.id=deals.vacancy_id " +
+            "INNER JOIN recruiters  ON recruiters.id=deals.recruiter_id " +
+            "WHERE applicants.id=#{applicantId}")
+    @Results({
+            @Result(column = "id", property = "id"),
+            @Result(column = "first_name", property = "firstName"),
+            @Result(column = "last_name", property = "lastName"),
+            @Result(column = "description", property = "description"),
+            @Result(column = "sex", property = "sex"),
+            @Result(column = "age", property = "age"),
+            @Result(column = "resume_file", property = "resumeFile"),
+            @Result(column = "test_answer_file", property = "testAnswerFile"),
+            @Result(column = "status", property = "status"),
+            @Result(column = "deal_id", property = "deal.id"),
+            @Result(column = "vacancy_id", property = "deal.vacancy.id"),
+            @Result(column = "employer_id", property = "deal.vacancy.employer.id"),
+            @Result(column = "recruiter_id", property = "deal.recruiter.id")
+    })
+    Applicant getApplicantById(final Long applicantId);
+
+    @Select("SELECT applicants.*, " +
+            "vacancies.id as vacancy_id, " +
+            "vacancies.employer_id, " +
+            "recruiters.id as recruiter_id " +
+            "FROM applicants " +
+            "INNER JOIN deals ON deals.id=applicants.deal_id " +
+            "INNER JOIN vacancies ON vacancies.id=deals.vacancy_id " +
+            "INNER JOIN recruiters  ON recruiters.id=deals.recruiter_id " +
+            "WHERE applicants.deal_id=#{dealId}")
+    @Results({
+            @Result(column = "id", property = "id"),
+            @Result(column = "first_name", property = "firstName"),
+            @Result(column = "last_name", property = "lastName"),
+            @Result(column = "description", property = "description"),
+            @Result(column = "sex", property = "sex"),
+            @Result(column = "age", property = "age"),
+            @Result(column = "resume_file", property = "resumeFile"),
+            @Result(column = "test_answer_file", property = "testAnswerFile"),
+            @Result(column = "status", property = "status"),
+            @Result(column = "deal_id", property = "deal.id"),
+            @Result(column = "vacancy_id", property = "deal.vacancy.id"),
+            @Result(column = "employer_id", property = "deal.vacancy.employer.id"),
+            @Result(column = "recruiter_id", property = "deal.recruiter.id")
+    })
+    public List<Applicant> getApplicantByDealId(final Long dealId);
+
+    //TODO
+    Boolean saveApplicant(final Applicant applicant);
+}
