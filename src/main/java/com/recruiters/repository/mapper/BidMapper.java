18bf6031b9c4385d94b15422dfa1bafeb7e94feb
Dmitry Shmatko <leodex23@gmail.com>
2013-12-28 13:05:06 +0700

    Cosmetic code styling changes

--- src/main/java/com/recruiters/repository/mapper/BidMapper.java
+++ src/main/java/com/recruiters/repository/mapper/BidMapper.java
@@ -1,7 +1,6 @@
 package com.recruiters.repository.mapper;
 
 import com.recruiters.model.Bid;
-import com.recruiters.model.Employer;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
--- src/main/java/com/recruiters/repository/mapper/DealMapper.java
+++ src/main/java/com/recruiters/repository/mapper/DealMapper.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Mapper for Deal POJO
  */
 public interface DealMapper {
 
