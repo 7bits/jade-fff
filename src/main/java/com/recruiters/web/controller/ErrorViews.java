5f79fd94b93b967bde17d1ccde0e68235b7f066b
Dmitry Shmatko <leodex23@gmail.com>
2014-02-10 11:47:54 +0700

    Web controller slightly refactored

--- src/main/java/com/recruiters/web/controller/ErrorViews.java
+++ src/main/java/com/recruiters/web/controller/ErrorViews.java
@@ -1,4 +1,4 @@
-package com.recruiters.web.controller.utils;
+package com.recruiters.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
