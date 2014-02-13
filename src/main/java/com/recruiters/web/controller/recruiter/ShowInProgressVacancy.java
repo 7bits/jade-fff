a8da2e3b4554eebd14dcc87a9e4caed7528920d0
Dmitry Shmatko <leodex23@gmail.com>
2014-02-13 14:04:37 +0700

    Now recruiter can leave feedback when Deal is ended too

--- src/main/java/com/recruiters/web/controller/recruiter/ShowInProgressVacancy.java
+++ src/main/java/com/recruiters/web/controller/recruiter/ShowInProgressVacancy.java
@@ -7,18 +7,22 @@ import com.recruiters.service.exception.NotFoundException;
 import com.recruiters.service.RecruiterService;
 import com.recruiters.service.exception.ServiceException;
 import com.recruiters.web.controller.utils.UserUtils;
+import com.recruiters.web.helper.UrlResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 /**
- * Show progress of vacancy (deal) for recruiter
+ * Show progress of vacancy (deal) for recruiter and all related actions
  */
 @Controller
 public class ShowInProgressVacancy {
@@ -29,6 +33,9 @@ public class ShowInProgressVacancy {
     /** Recruiter Service provides all Recruiter related methods */
     @Autowired
     private RecruiterService recruiterService = null;
+    /** Url Builder */
+    @Autowired
+    private UrlResolver urlResolver;
 
     /**
      * Displays deal for certain vacancy
@@ -65,6 +72,41 @@ public class ShowInProgressVacancy {
         }
 
         return vacancyInProgress;
+    }
+
+    /**
+     * Leave feedback for deal ended
+     * @param feedback    Feedback
+     * @param dealId      Id of deal
+     * @param request     Http request
+     * @param response    Http response
+     * @return redirect to "vacancies list" page if everything goes fine,
+     * Forbidden page if this deal does not belong to this recruiter,
+     * Internal Server Error page if something is wrong with obtaining data
+     * due to technical or any other reasons
+     * @throws Exception in very rare circumstances: it should be runtime
+     * or servlet Exception to be thrown
+     */
+    @RequestMapping(value = "/recruiter-deal-leave-feedback", method = RequestMethod.POST)
+    public String leaveFeedback(
+            @RequestParam(value = "feedback", required = false) final String feedback,
+            @RequestParam(value = "id", required = true) final Long dealId,
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws Exception {
+        try {
+            User user = userUtils.getCurrentUser(request);
+            recruiterService.leaveFeedback(dealId, feedback, user.getRecruiterId());
+        } catch (NotAffiliatedException e) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return null;
+        } catch (ServiceException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return null;
+        }
+        Locale locale = RequestContextUtils.getLocale(request);
+
+        return  urlResolver.buildRedirectUri("recruiter-active-deals", locale);
     }
 
     public RecruiterService getRecruiterService() {
